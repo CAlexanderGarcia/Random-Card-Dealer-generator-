58f3cab7bb1a7ad2f17a777bf722b59e0fad538f
CAlexanderGarcia <calexandergarcia@gmail.com>
2021-10-23 17:47:43 +0000

    Trabajo Terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function cardBuilder() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let symbols = ["♦", "♥", "♠", "♣"];
+  let number = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector(".number").innerHTML = numbers[number]; //elemento del array aleatorio
+
+  let symbol = Math.floor(Math.random() * symbols.length);
+
+  document.querySelector(".icon-left").innerHTML = symbols[symbol];
+  document.querySelector(".icon-right").innerHTML = symbols[symbol];
 };
